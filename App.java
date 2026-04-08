e831e6541c590f33a7d160ea62184bc747395c8d
manyasharma1008 <manya.s1008@gmail.com>
2026-04-08 14:53:44 +0530

    UC5 added by Manya

--- App.java
+++ App.java
@@ -1,36 +1,27 @@
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-
-public class App {
+public class UseCase5TrainConsistMgmt {
     public static void main(String[] args) {
 
-        System.out.println("=====================================");
-        System.out.println("UC4 - Maintain Ordered Bogie Consist");
-        System.out.println("=====================================\n");
+        System.out.println("***************************************");
+        System.out.println(" UC5 - Preserve Insertion Order of Bogies ");
+        System.out.println("***************************************\n");
 
-        List<String> trainConsist = new LinkedList<>();
+        Set<String> formation = new LinkedHashSet<>();
 
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
 
-        System.out.println("Initial Train Consist:");
-        System.out.println(trainConsist + "\n");
+        formation.add("Sleeper");
+        System.out.println("Final Train Formation:");
+        System.out.println(formation);
 
-        trainConsist.add(2, "Pantry Car");
+        System.out.println("\nNote:");
+        System.out.println("LinkedHashSet preserves insertion order and removes duplicates automatically.");
 
-        System.out.println("After Inserting 'Pantry Car' at position 2:");
-        System.out.println(trainConsist + "\n");
-
-        ((LinkedList<String>) trainConsist).removeFirst();
-        ((LinkedList<String>) trainConsist).removeLast();
-
-        System.out.println("After Removing First and Last Bogie:");
-        System.out.println(trainConsist + "\n");
-
-        System.out.println("UC4 ordered consist operations completed...");
+        System.out.println("\nUC5 formation setup completed...");
     }
 }
