6a5919d782dbfaed5b977a0478aefc18da91aaaa
manyasharma1008 <manya.s1008@gmail.com>
2026-04-24 14:02:46 +0530

    Manya added UC19

--- App.java
+++ App.java
@@ -1,35 +1,48 @@
+import java.util.Arrays;
 
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("==============================================");
-        System.out.println("Use Case 18 - Linear Search for Bogie ID");
-        System.out.println("==============================================\n");
+        System.out.println("===============================================");
+        System.out.println("UC19 - Binary Search for Bogie ID");
+        System.out.println("===============================================\n");
 
-        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
+        String[] bogieIds = {"BG309", "BG101", "BG550", "BG205", "BG412"};
+
+        Arrays.sort(bogieIds);
 
         String searchId = "BG309";
 
-        System.out.println("Available Bogie IDs:");
+        System.out.println("Sorted Bogie IDs:");
         for (String id : bogieIds) {
             System.out.println(id);
         }
 
-        boolean found = linearSearch(bogieIds, searchId);
+        boolean found = binarySearch(bogieIds, searchId);
 
         if (found) {
-            System.out.println("\nBogie " + searchId + " found in train consist.");
+            System.out.println("\nBogie " + searchId + " found using Binary Search.");
         } else {
             System.out.println("\nBogie " + searchId + " not found in train consist.");
         }
 
-        System.out.println("\nUC18 search completed...");
+        System.out.println("\nUC19 search completed...");
     }
 
-    public static boolean linearSearch(String[] arr, String key) {
-        for (String id : arr) {
-            if (id.equals(key)) {
+    public static boolean binarySearch(String[] arr, String key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int cmp = arr[mid].compareTo(key);
+
+            if (cmp == 0) {
                 return true;
+            } else if (cmp < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return false;
